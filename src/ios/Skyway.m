1e9cee7e7cb8bce91eec003e1758116da296aa00
TuanPA <tuanpa@sis-vn.com>
2019-03-25 13:35:59 +0700

    fire event fix

--- src/ios/Skyway.m
+++ src/ios/Skyway.m
@@ -38,22 +38,38 @@
     ViewController *vc = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
     vc.myId = myId;
     vc.partnerId = partnerId;
+
     [vc setSuccessBlock:^(NSUInteger start, NSUInteger end) {
         NSLog(@"block call: %lu %lu", start, end);
         NSString *startStr = [[NSString alloc] initWithFormat:@"%lu", (unsigned long)start];
         NSString *endStr = [[NSString alloc] initWithFormat:@"%lu", (unsigned long)end];
 
         NSDictionary *timeDict = @{@"startTime": startStr, @"endTime": endStr};
-        [self fireEvent:@"" event:@"skyway_hangup" withData:timeDict];
+        
+        [self fireEvent:@"" event:@"skyway_hangup" withData:[self generateJsonStringFromDictionary:timeDict]];
 
         // CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:timeDict];
         // [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-        // }];
-
+        }];
     [ROOTVIEW presentViewController:vc animated:YES completion:^{}];
 }
 
-- (void) fireEvent:(NSString *)obj event:(NSString *)eventName withData:(NSString *)jsonStr {
+- (NSString *)generateJsonStringFromDictionary:(NSDictionary *)dict {
+    NSError *error;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dict
+                                                       options:NSJSONWritingPrettyPrinted // Pass 0 if you don't care about the readability of the generated string
+                                                         error:&error];
+    
+    if (! jsonData) {
+        NSLog(@"Got an error: %@", error);
+        return @"";
+    } else {
+        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+        return jsonString;
+    }
+}
+
+- (void)fireEvent:(NSString *)obj event:(NSString *)eventName withData:(NSString *)jsonStr {
     NSString* js;
     if(obj && [obj isEqualToString:@"window"]) {
         js = [NSString stringWithFormat:@"var evt=document.createEvent(\"UIEvents\");evt.initUIEvent(\"%@\",true,false,window,0);window.dispatchEvent(evt);", eventName];
@@ -64,6 +80,5 @@
     }
     [self.commandDelegate evalJs:js];
 }
-
 
 @end
